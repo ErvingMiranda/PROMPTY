3c49f5c51edee84842763c04b86c87ff31555775
Ezone <ervingmr@uamv.edu.ni>
2025-11-30 12:07:37 -0600

    Enable smart chat actions via API

---
+++ PROMPTY_3.0/prompty_core/__init__.py
@@ -0,0 +1,5 @@
+"""Componentes reutilizables de PROMPTY (núcleo compartido)."""
+
+from .comandos import ACCIONES_DISPONIBLES, ejecutar_accion
+
+__all__ = ["ACCIONES_DISPONIBLES", "ejecutar_accion"]
